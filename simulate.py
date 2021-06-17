eb305ec1b20a0a469fba537af3a22fc0f438cb1f
Michał Kuklewski <michael.kuklewski@gmail.com>
2021-06-18 00:12:23 +0200

    Migen and Cocotb

--- simulate.py
+++ simulate.py
@@ -4,7 +4,9 @@ import os
 
 import subprocess
 
+dartiq_run_bypass = "./simulate "
 
+test_output = {}
 
 if __name__ == "__main__":
     print("Start Simulations!")
@@ -14,30 +16,46 @@ if __name__ == "__main__":
 
     print(data)
 
+    if not os.path.exists("temp"):
+        status = subprocess.run(["mkdir temp"], shell=True)
 
     for sim_name in data["simulation_list"]:
-        print(sim_name)
+
+        print("INFO: Simulation name: " + sim_name)
         if data["simulation_list"][sim_name]["sim_type"] == "Migen":
-            print("Migen type simulation")
-            # cmd = ["dartiq",\
-            #     "run", \
-            #     "--no-stdin",\
-            #     '\"python',
-            #     './modules/ELHEP_Cores/elhep_cores/cores/dsp/baseline_avg.py\"']
+            print("INFO: Migen type simulation.")
+            test_output["simulation"] = sim_name
+            # test_output[sim_name]["type"] = data["simulation_list"][sim_name]["sim_type"]
+
+            cmd = [dartiq_run_bypass + data["simulation_list"][sim_name]["tb_path"]]
+
+            subprocess.run(cmd, shell=True)
+
+        if data["simulation_list"][sim_name]["sim_type"] == "Cocotb":
+
+            # cmd = [dartiq_run_bypass + data["simulation_list"][sim_name]["tb_path"]]
+
+            # subprocess.run(cmd, shell=True)
+            subprocess.run("cd modules/ELHEP_Cores/elhep_cores/cores/dsp/tests; make", shell=True)
+
+            # cmd.append("cd temp;")
+            # cmd.append("ls;")
+
+            # cmd.append("dartiq run --no-stdin \"python ../modules/ELHEP_Cores/elhep_cores/cores/dsp/baseline_avg.py\"")
 
 
-            # cmd = ["ls"]
+            # pwd = os.path.dirname(os.path.realpath(__file__)) 
 
-            # cmd = ["dartiq", "run", "\"python ./modules/ELHEP_Cores/elhep_cores/cores/dsp/baseline_avg.py\""]
+            # print(pwd)
+            # status = subprocess.run(["ls"], shell=True)
+            # status = subprocess.run(["ls; cd temp; ls; dartiq run --no-stdin \"python ../modules/ELHEP_Cores/elhep_cores/cores/dsp/baseline_avg.py\" "], shell=True)
+            # 
 
-            # cmd = "dartiq run --no-stdin \"python ./modules/ELHEP_Cores/elhep_cores/cores/dsp/baseline_avg.py\"".split()
+    # print(status)
 
-            cmd = "dartiq run \"echo test\"".split()
-            print(cmd)
 
-            pwd = os.path.dirname(os.path.realpath(__file__)) 
 
-            print(pwd)
-            status = subprocess.run(cmd, shell=True)
 
-    print(status)
+
+
+
